5cba666ff090f51ad8cc6eeee69e021ec7539ac6
antmoute <lejugeti@gmail.com>
2025-04-13 15:43:24 +0200

    default client max stories fetched number to 10

--- client/main.go
+++ client/main.go
@@ -78,7 +78,7 @@ const whoisFlag string = "whois"
 var (
     userName = flag.String(whoisFlag, "", "Retrieve information on user passed as input")
     isListMode = flag.Bool(listFlag, false, "Number of top news from HackerNews front page to fetch")
-    newsNumber = flag.Int(newsNumberFlag, 30, fmt.Sprintf("Max number of news to fetch. Must be used along with the -%s flag", listFlag))
+    newsNumber = flag.Int(newsNumberFlag, 10, fmt.Sprintf("Max number of news to fetch. Must be used along with the -%s flag", listFlag))
     timeoutSeconds = flag.Int(timeoutFlag, 20, "Timeout in seconds before client cutting connection to server")
 )
 
